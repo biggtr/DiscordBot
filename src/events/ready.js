b15307cc540ec6cbe2456551d1bf52ae11efd353
biggtr <bigtorresg@gmail.com>
2024-08-18 16:38:47 +0300

    feat: add eventHandler and getAllFiles.js to handle commands

---
+++ src/events/ready.js
@@ -0,0 +1,12 @@
+const {Events} = require("discord.js");
+
+module.exports =
+{
+    name: Events.ClientReady,
+    once: true,
+    execute(readyClient)
+    {
+        console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    }
+
+};
---
+++ src/main.js
@@ -0,0 +1,19 @@
+const dotenv =  require('dotenv'); 
+dotenv.config();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const getAllFiles = require('./utils/getAllFiles');
+const eventHandler = require('./handlers/eventHandler');
+
+
+const client = new Client({intents :[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMembers,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
+
+//add commands property to client to store the commands retrieved;
+client.commands = new Collection();
+
+
+
+eventHandler(client);
+
+client.login(process.env.DISCORD_TOKEN);
