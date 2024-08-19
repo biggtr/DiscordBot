82fc27aeeeeaaa1dd4538b315a9d0a70e21b6512
biggtr <bigtorresg@gmail.com>
2024-08-19 17:03:56 +0300

    feat: add registerCommands that handles everything has to do with register/edit/delete commands

--- src/events/ready/registerCommands.js
+++ src/events/ready/registerCommands.js
@@ -3,23 +3,55 @@ const getLocalCommands = require("../../utils/getLocalCommands")
 const dotenv =  require('dotenv'); 
 
 dotenv.config();
-module.exports = (client) => 
-{
-    try 
-    {    
-        const localCommands =  getLocalCommands()
-        const applicationCommands = getApplicationCommnads(client,process.env.GUILD_ID)
-        for(localCommand of localCommands)
-        {
-            const {name, description, options} = localCommand.data;
-            console.log('Command Name:', name);
-            console.log('Description:', description);
-            console.log('Options:', options);
-        }
-    }
 
-    catch (error)
-    {
-        console.log(`There was an error : ${error}`);
+module.exports = async (client) => {
+    try {
+      const localCommands = getLocalCommands();
+      const applicationCommands = await getApplicationCommands(
+        client,
+        process.env.GUILD_ID
+      );
+  
+      for (const localCommand of localCommands) {
+        const { name, description, options } = localCommand;
+  
+        const existingCommand = await applicationCommands.cache.find(
+          (cmd) => cmd.name === name
+        );
+  
+        if (existingCommand) {
+          if (localCommand.deleted) {
+            await applicationCommands.delete(existingCommand.id);
+            console.log(`🗑 Deleted command "${name}".`);
+            continue;
+          }
+  
+          if (areCommandsDifferent(existingCommand, localCommand)) {
+            await applicationCommands.edit(existingCommand.id, {
+              description,
+              options,
+            });
+  
+            console.log(`🔁 Edited command "${name}".`);
+          }
+        } else {
+          if (localCommand.deleted) {
+            console.log(
+              `⏩ Skipping registering command "${name}" as it's set to delete.`
+            );
+            continue;
+          }
+  
+          await applicationCommands.create({
+            name,
+            description,
+            options,
+          });
+  
+          console.log(`👍 Registered command "${name}."`);
+        }
+      }
+    } catch (error) {
+      console.log(`TThere was an error: ${error}`);
     }
-}
+  };
