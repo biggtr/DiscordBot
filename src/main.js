c1c94df62e045ac9e8da9f16d0ebcd26b4c12e29
biggtr <bigtorresg@gmail.com>
2024-08-21 12:21:08 +0300

    feat: add mongoDb and connection string - DB is online

--- src/main.js
+++ src/main.js
@@ -5,14 +5,28 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const getAllFiles = require('./utils/getAllFiles');
 const eventHandler = require('./handlers/eventHandler');
+const { default: mongoose } = require('mongoose');
 
 
 const client = new Client({intents :[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMembers,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
 
-//add commands property to client to store the commands retrieved;
-client.commands = new Collection();
 
+(
+    async() => 
+    {
+        try
+        {
+            mongoose.set("strictQuery", false);
+            await mongoose.connect(process.env.MONGODB_URI);
+            console.log("Connected To DB...");
 
-eventHandler(client);
+            eventHandler(client);
+            client.login(process.env.DISCORD_TOKEN);
+        }
+        catch(error)
+        {
+            console.log(`There is an error ${error}`)
+        }
+    }
+)();
 
-client.login(process.env.DISCORD_TOKEN);
