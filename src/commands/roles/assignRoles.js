a579c3349f166cb2076dc2caa5b3490eb382852d
biggtr <bigtorresg@gmail.com>
2024-08-22 16:27:37 +0300

    fix: resolve issue with enteredRole was saved as an id not a name in db

--- src/commands/roles/assignRoles.js
+++ src/commands/roles/assignRoles.js
@@ -2,7 +2,7 @@ const {Client, Interaction, ApplicationCommandOptionType,PermissionFlagsBits} = 
 const User = require("../../models/User")
 const Role = require("../../models/Role")
 const Portfolio = require("../../models/Portfolio")
-
+const cooldowns = new Map();
 module.exports = 
 {
     /**
@@ -13,8 +13,21 @@ module.exports =
     callback: async (client, interaction) =>
     {
         targetUserId = interaction.member.id;
-        enteredRole = interaction.options.get("role").value;
+        enteredRole = interaction.options.get("role").role;
         enteredPortfolio = interaction.options.get("portfolio").value;
+        const cooldownPeriod = 1000 * 60 * 60 * 24 * 7; // 1 week in milliseconds
+
+        const now = Date.now();
+        await interaction.deferReply();
+
+        if (cooldowns.has(targetUserId)) {
+            const expirationTime = cooldowns.get(targetUserId);
+            if (now < expirationTime) {
+                const remainingTime = Math.ceil((expirationTime - now) / (cooldownPeriod /7)); // convert to days
+                await interaction.editReply(`Please wait ${remainingTime} day(s) before using this command again.`);
+                return;
+            }
+        }
 
         const query = 
         {
@@ -28,14 +41,13 @@ module.exports =
             .populate("portfolio");
             let newRole;
             let newPortfolio;
-            await interaction.deferReply();
             if(userObject)
             {
-                const existingRole = await Role.findOne({ roleName: enteredRole }) ;
+                const existingRole = await Role.findOne({ roleName: enteredRole.name }) ;
                 const existingPortfolio = await Portfolio.findOne({ link: enteredPortfolio });
 
-                const newRole = !existingRole ? await Role.create({ roleName: enteredRole }) : existingRole;
-                const newPortfolio = !existingPortfolio ? await Portfolio.create({ link: enteredPortfolio }) : existingPortfolio;
+                const newRole = !existingRole ? await Role.create({ roleName: enteredRole.name }) : existingRole;
+                const newPortfolio = !existingPortfolio ? await Portfolio.create({ link: enteredRole.name }) : existingPortfolio;
 
                 const isRoleSame = userObject.role ? userObject.role.equals(newRole._id) : false;
                 const isPortfolioSame = userObject.portfolio ? userObject.portfolio.equals(newPortfolio._id) : false;
@@ -52,11 +64,11 @@ module.exports =
                     return;
                 });
                 
-                await interaction.editReply(`You're now Assigned to ${enteredRole} role.`);
+                await interaction.editReply(`You're now Assigned to ${enteredRole.name} role.`);
             }
             else
             {
-                newRole = await Role.create({ roleName: enteredRole });
+                newRole = await Role.create({ roleName: enteredRole.name });
                 newPortfolio = await Portfolio.create({ link: enteredPortfolio });
         
                 const newUser = await User.create({
@@ -66,10 +78,11 @@ module.exports =
                     portfolio: newPortfolio,
                     role: newRole
                 });
-                await interaction.editReply(`You're now assigned to the ${enteredRole} role.`);
+                await interaction.editReply(`You're now assigned to the ${enteredRole.name} role.`);
 
                 await newUser.save();
             }
+            cooldowns.set(targetUserId, now + cooldownPeriod);
         }
         catch(error)
         {
